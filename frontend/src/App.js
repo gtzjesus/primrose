7b658947207cee2bf7071cce462d2c45853566f9
Jesus Gutierrez <gtz.jesus@outlook.com>
2022-03-21 13:06:48 -0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">primrose</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
